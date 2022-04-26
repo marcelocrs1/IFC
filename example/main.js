1e0ab0fc7224e82ca76599f15d1e6f40ccd6afc4
Marcelo Santos <msantos@bimms.net>
2022-04-26 15:48:10 +0100

    Preparando para colocar online

--- example/main.js
+++ example/main.js
@@ -30,7 +30,7 @@ import Stats from 'stats.js/src/Stats';
 /**/
 
 const container = document.getElementById('viewer-container');
-const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xc4d3d2) });
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0x2d3b40) });
 // viewer.axes.setAxes();
 // viewer.grid.setGrid();
 viewer.shadowDropper.darkness = 1.5;
